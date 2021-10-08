4522352c75d6385c90a2060381283d24b24a422b
Benjamin Albanese <b.albanese@levigo.de>
2021-10-09 01:09:48 +0200

    selectable list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
 
 @Component({
@@ -7,8 +8,13 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  hero: Hero = {id: 1, name: 'Windstorm'};
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 
   constructor() {
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Batman', archEnemy: 'Joker'},
+  {id: 12, name: 'Superman', archEnemy: 'Lex Luthor'},
+  {id: 13, name: 'Wonder Woman', archEnemy: ''},
+  {id: 14, name: 'Batgirl', archEnemy: 'Harley Quinn'},
+  {id: 15, name: 'Ironman', archEnemy: 'Thanos'},
+  {id: 16, name: 'Pepper', archEnemy: 'Tony'},
+  {id: 17, name: 'Captain America', archEnemy: 'Hydra'},
+  {id: 18, name: 'Black Widow', archEnemy: 'Russians'},
+  {id: 19, name: 'Thor', archEnemy: 'Loki'},
+  {id: 20, name: 'Hulk', archEnemy: 'Banner'},
+]
