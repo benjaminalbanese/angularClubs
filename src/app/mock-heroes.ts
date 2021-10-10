e63a5c082e01ff2177df6c93e6caa5dc2b8a845a
Benjamin Albanese <b.albanese@levigo.de>
2021-10-10 22:39:27 +0200

    add the hero-detail component

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,12 +3,12 @@ import {Hero} from "./hero";
 export const HEROES: Hero[] = [
   {id: 11, name: 'Batman', archEnemy: 'Joker'},
   {id: 12, name: 'Superman', archEnemy: 'Lex Luthor'},
-  {id: 13, name: 'Wonder Woman', archEnemy: ''},
+  {id: 13, name: 'Wonder Woman', archEnemy: 'Ares'},
   {id: 14, name: 'Batgirl', archEnemy: 'Harley Quinn'},
   {id: 15, name: 'Ironman', archEnemy: 'Thanos'},
-  {id: 16, name: 'Pepper', archEnemy: 'Tony'},
+  {id: 16, name: 'Pepper', archEnemy: 'Aldrich Killian'},
   {id: 17, name: 'Captain America', archEnemy: 'Hydra'},
-  {id: 18, name: 'Black Widow', archEnemy: 'Russians'},
+  {id: 18, name: 'Black Widow', archEnemy: 'Anton Vanko'},
   {id: 19, name: 'Thor', archEnemy: 'Loki'},
   {id: 20, name: 'Hulk', archEnemy: 'Banner'},
 ]
