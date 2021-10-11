cf53d1f8ec1cb731bf39d7143c71cf3846d14496
Benjamin Albanese <b.albanese@levigo.de>
2021-10-11 12:27:59 +0200

    create and display messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 import {HeroService} from "./hero.service";
+import {MessageService} from "../messages/message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -13,17 +14,18 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero with id=${hero.id}`);
   }
 
 
-  constructor(private heroService : HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
     this.getHeroes()
   }
 
-  getHeroes(): void{
+  getHeroes(): void {
     this.heroService.getHeroes().subscribe(fetched => this.heroes = fetched);
   }
 }
