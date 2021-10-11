7529c40e742ba603b7966d23ce4e7af4bcedeef1
Benjamin Albanese <b.albanese@levigo.de>
2021-10-11 19:32:02 +0200

    fetch data from the server (see github firstreact project)

--- src/app/clubs/club.service.ts
+++ src/app/clubs/club.service.ts
@@ -3,26 +3,32 @@ import {CLUBS} from "../mock-clubs";
 import {Observable, of} from "rxjs";
 import {MessageService} from "../messages/message.service";
 import {Club} from "../club";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClubService {
 
-  constructor(private messageService : MessageService) {
+  private clubsUrl = 'http://localhost:8080/getClubList'
+
+  constructor(private http: HttpClient, private messageService: MessageService) {
+  }
+
+  private log(message: string) {
+    this.messageService.add(`ClubService: ${message}`);
   }
 
   getClubs(): Observable<Club[]> {
-    const clubs = of(CLUBS);
-    this.messageService.add('ClubService: fetched clubs');
-    return clubs
+    this.log('ClubService: fetching clubs');
+    return this.http.get<Club[]>(this.clubsUrl);
   }
 
-  getClub(id : number): Observable<Club>{
+  getClub(id: number): Observable<Club> {
     // For now, assume that a club with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const club = CLUBS.find(h => h.id === id)!;
-    this.messageService.add(`Club-Service: fetched club id = ${id}`);
+    this.log(`Club-Service: fetched club id = ${id}`)
     return of(club);
   }
 }
